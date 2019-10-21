cbf2359d2f6ff31fb4e4f5374620cb3643504384
kao-tsai <7895123speedkills@gmail.com>
2019-10-22 06:52:11 +0800

    final

--- plot/disk.sh
+++ plot/disk.sh
@@ -12,7 +12,7 @@ set terminal png
 set key right
 set output 'DiskFigure/random_disk_result.png'
 
-plot [0:100][20000:40000]'random_dis_result/fifo_dis_rand.txt' using 1:2 with linespoints linewidth 2 title 'FIFO',\
+plot [0:100][20000:34000]'random_dis_result/fifo_dis_rand.txt' using 1:2 with linespoints linewidth 2 title 'FIFO',\
 'random_dis_result/opt_dis_rand.txt' using 1:2 with linespoints linewidth 2 title 'opt',\
 'random_dis_result/esc_dis_rand.txt' using 1:2 with linespoints linewidth 2 title 'esc',\
 'random_dis_result/lrusc_dis_rand.txt' using 1:2 with linespoints linewidth 2 title 'lrusc'
@@ -35,7 +35,7 @@ plot [0:100][10000:32000]'locality_dis_result/fifo_dis_locality.txt' using 1:2 w
 
 
 reset
-set title "disk write/frame num(locality)"
+set title "disk write/frame num(my_ref)"
 set xlabel "frame num"
 set ylabel "disk write"
 set xtics 10
